a017ea651f8ce459c03d728bfa8a07045d78be49
ToniIltanen <toni.iltanen@codecontrol.fi>
2021-09-25 02:33:29 +0300

    added websocket tests

--- test/routes.test.js
+++ test/routes.test.js
@@ -2,6 +2,7 @@ var config = require("../config/settings.json")
 var http = require('http')
 var log4js = require('log4js');
 var sinon = require('sinon');
+var ws = require('ws')
 var chai = require('chai')
 chai.use(require('chai-http'))
 var gatewayUrl = 'http://localhost:' + config.settings.server.port
@@ -10,7 +11,8 @@ sinon.stub(log4js, "getLogger").returns({
   info: function () { },
   error: function () { }
 })
-var s = require('../server.js')
+var s = require('../server.js');
+const { expect } = require("chai");
 var config = s.configure()
 var server = s.server(config)
 
@@ -29,8 +31,8 @@ describe('node-gateway', function () {
       chai.request(gatewayUrl)
         .get('/')
         .end(function (err, res) {
-          expect(res.error.text).toBe(config.settings.server.noRouteMatchesErrorMessage)
-          expect(res.statusCode).toBe(400);
+          expect(res.error.text).to.equal(config.settings.server.noRouteMatchesErrorMessage)
+          expect(res.statusCode).to.equal(400);
           done();
         });
     })
@@ -38,8 +40,8 @@ describe('node-gateway', function () {
       chai.request(gatewayUrl)
         .get('/error')
         .end(function (err, res) {
-          expect(res.error.text).toBe(config.settings.server.generalErrorMessage)
-          expect(res.statusCode).toBe(500)
+          expect(res.error.text).to.equal(config.settings.server.generalErrorMessage)
+          expect(res.statusCode).to.equal(500)
           done()
         })
     })
@@ -47,9 +49,25 @@ describe('node-gateway', function () {
       chai.request(gatewayUrl)
         .options('/users')
         .end(function (err, res) {
-          expect(res.statusCode).toBe(200)
+          expect(res.statusCode).to.equal(200)
           done()
         })
+    })
+  })
+  describe('Websocket basic tests', function () {
+    it('Should return 500 with invalid connection upgrade', function(done) {
+      const client = new ws("ws://localhost:6010/notifications")
+      client.on('error', (err) => {
+        expect(err.toString()).to.equal("Error: Unexpected server response: 500")
+        done()
+      })
+    }),
+    it('Should return 400 with invalid websocket route', function(done) {
+      const client = new ws("ws://localhost:6010/wrong_path")
+      client.on('error', (err) => {
+        expect(err.toString()).to.equal("Error: Unexpected server response: 400")
+        done()
+      })
     })
   })
 })
