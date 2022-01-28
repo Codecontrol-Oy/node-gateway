b18799afb66a6a46e53f528ab2c2e42784f13ba8
ToniIltanen <toni.iltanen@codecontrol.fi>
2022-01-28 13:08:26 +0200

    Remove of jasmine reports

--- jasmine.js
+++
@@ -1,9 +0,0 @@
-var jasmineReporters = require('jasmine-reporters');
-jasmine.VERBOSE = true;
-jasmine.getEnv().addReporter(
-  new jasmineReporters.JUnitXmlReporter({
-    consolidateAll: false,
-    savePath: "report/",
-    filePrefix: 'test-results'
-  })
-)
